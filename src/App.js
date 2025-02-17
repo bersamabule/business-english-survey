fcbc78d443e561b9497d8fd98852d7a13cb40913
bersamabule <ahymers1867@gmail.com>
2025-02-17 16:35:17 +0700

    Add routing and configure for /survey path

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Survey from './components/Survey';
 
 function App() {
   return (
-    <Box sx={{ 
-      minHeight: '100vh',
-      backgroundColor: '#f5f5f5',
-      py: 4
-    }}>
-      <Survey />
-    </Box>
+    <Router>
+      <Box sx={{ 
+        minHeight: '100vh',
+        backgroundColor: '#f5f5f5',
+        py: 4
+      }}>
+        <Routes>
+          <Route path="/survey" element={<Survey />} />
+          <Route path="/" element={<Navigate to="/survey" replace />} />
+        </Routes>
+      </Box>
+    </Router>
   );
 }
 
